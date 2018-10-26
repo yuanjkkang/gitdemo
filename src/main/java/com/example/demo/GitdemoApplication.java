aff1dad182e118f0276899e67977c478e3edfe57
袁继康 <jikang.yuan@bangdao-tech.com>
2018-10-26 14:28:39 +0800

    test

--- src/main/java/com/example/demo/GitdemoApplication.java
+++ src/main/java/com/example/demo/GitdemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitdemoApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(GitdemoApplication.class, args);
 	}
